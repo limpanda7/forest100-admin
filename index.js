cbcb24c33b318717637a0e8c8e896807f38b53fc
Sunghoon Lim <limpanda7@gmail.com>
2022-02-13 23:58:26 +0900

    - 최초 라우팅

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 // express 모듈 불러오기
 const express = require('express');
+const path = require('path');
 
 // express 객체 생성
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 // 기본 포트를 app 객체에 설정
 const port = process.env.PORT || 5000;
 app.listen(port);
-
-// 미들웨어 함수를 특정 경로에 등록
-app.use('/api/data', function(req, res) {
-    res.json({ greeting: 'Hello World' });
-});
-
 console.log(`server running at http ${port}`);
